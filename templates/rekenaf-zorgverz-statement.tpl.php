ab440c3586510c5cbe4d7a545775a1f72eb250e5
Thomas de Graaff <tdgraaff@sp.nl>
2015-03-17 15:00:23 +0100

    Adds caching for steunbetuigingen.

--- templates/rekenaf-zorgverz-statement.tpl.php
+++ templates/rekenaf-zorgverz-statement.tpl.php
@@ -1,23 +1,27 @@
 <li id="statement_<?php print $statement->stid; ?>" class="statement">
   <div class="inner-container">
     <div class="inner-wrapper">
-      <?php $name = ($statement->anonymous) ? 'Anonieme steunbetuiging' : check_plain($statement->name) ?>
+      <?php $name = check_plain($statement->name) ?>
       <p><?php print $name;?></p>
       <?php
         // Get image if available.
         if (!empty($statement->fid)) {
           $file = file_load($statement->fid);
-          $image = image_load($file->uri);
-          $content = array(
-            'file' => array(
-              '#theme' => 'image_style',
-              '#style_name' => 'media_thumbnail',
-              '#path' => $image->source,
-            ),
-          );
-          $image_output = drupal_render($content);
+          if ($file) {
+            $image = image_load($file->uri);
+            if ($image) {
+              $content = array(
+                'file' => array(
+                  '#theme' => 'image_style',
+                  '#style_name' => 'media_thumbnail',
+                  '#path' => $image->source,
+                ),
+              );
+              $image_output = drupal_render($content);
+            }
+          }
         }
-        else {
+        if (empty($image_output)) {
           $module_path = drupal_get_path('module', 'rekenaf_zorgverz');
           $image_path = '/' . $module_path . '/images/anoniem.png';
           $image_output = '<img src="' . $image_path . '" width="100" height="100" />';
